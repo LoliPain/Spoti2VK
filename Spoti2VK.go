31c13cf728595959e5a2e0ebd9c510577b641803
P2LOVE <40074412+P2LOVE@users.noreply.github.com>
2020-03-12 06:07:16 +0400

    Revert "search count"
    This reverts commit dec447ef02acb37115975b9342fafba4f6cc1884.

--- Spoti2VK.go
+++ Spoti2VK.go
@@ -162,7 +162,7 @@ func VKQuery(SpotifyQuery string) interface{} {
 	r := url.Values{
 		"q":             {SpotifyQuery},
 		"auto_complete": {"1"},
-		"count":         {"4"},
+		"count":         {"10"},
 		"access_token":  {VKToken},
 		"v":             {"5.1"},
 	}
